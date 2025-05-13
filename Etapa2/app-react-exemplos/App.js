f7bae06ad4db11bb1d8a10d572f83b14818bf4ba
maryclrap <mp28052008@gmail.com>
2025-05-13 09:20:47 -0300

    TextInput basic examples

--- Etapa2/app-react-exemplos/App.js
+++ Etapa2/app-react-exemplos/App.js
@@ -2,60 +2,23 @@ import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Image, TextInput, FlatList } from 'react-native';
 
-import List from './components/List';
+import Inputs from './components/Inputs';
 
 export default function App() {
  
   return (
     <View style={styles.container}>
-      <List />
+      <Inputs />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 20,
-    marginTop: 60,
-  },
-  text: {
-    fontSize: 24,
-  },
-  buttonContainer: {
-    flexDirection: 'row',a
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-  },
-  list: {
-    marginTop: 20,
-  },
-  item: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'space-between',
-    marginBottom: 10,
-    padding: 10,
-    backgroundColor: '#f0f0f0',
-    borderRadius: 5,
-  },
-  itemText: {
-    flex: 1,
-    marginRight: 10,
-  },
-  buttons: {
-    flexDirection: 'row',
-  },
-  editInput: {
-    flex: 1,
-    marginRight: 10,
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingHorizontal: 10,
+    alignItems: 'center',
+    height: 600,
+    marginTop: 150,
   }
 });
