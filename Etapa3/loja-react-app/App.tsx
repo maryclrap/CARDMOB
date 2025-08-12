11a692efdfe4326192a6cc318a49031afe18c585
maryclrap <mp28052008@gmail.com>
2025-08-12 08:41:33 -0300

    Inclui ThemeContext

--- Etapa3/loja-react-app/App.tsx
+++ Etapa3/loja-react-app/App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
+import { ThemeProvider } from './src/contexts/ThemeContext';
 
 export default function App() {
   return (
+    <ThemeProvider>
     <HomeScreen/>
+    </ThemeProvider>
   );
 }
 
